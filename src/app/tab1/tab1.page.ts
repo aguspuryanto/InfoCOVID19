305826956bfac4548fde95ce1c7f7ca0d6284d16
aguspuryanto <aguspuryanto@gmail.com>
2020-03-30 13:13:03 +0700

    update

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-=======
-import { Component, Injectable } from '@angular/core';
 // import { HttpClient, HttpClientModule } from '@angular/common/http';
 // import { Http, HttpModule, Response } from '@angular/http';
-
 import { HttpClient } from '@angular/common/http';
 // import { Observable } from 'rxjs';
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/catch';
->>>>>>> 97c8d2ba6aea58fac49b13a9aecd08e676319768
 
 @Component({
   selector: 'app-tab1',
@@ -18,31 +13,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-<<<<<<< HEAD
-  constructor(private geolocation: Geolocation) {
-    this.getCurrentPosition();
-  }
-
-  getCurrentPosition(){
-    this.geolocation.getCurrentPosition().then((resp) => {
-      // resp.coords.latitude
-      // resp.coords.longitude
-     }).catch((error) => {
-       console.log('Error getting location', error);
-     });
-     
-     let watch = this.geolocation.watchPosition();
-     watch.subscribe((data) => {
-       console.log("watchPosition", data);
-      // data can be a set of coordinates, or an error (if an error occurred).
-      // data.coords.latitude
-      // data.coords.longitude
-     });
-=======
   countryall: any;
-  countryid: any;
-  
+  countryid: any;  
   constructor(public http: HttpClient) {
 
     this.getRemoteData().subscribe(data => {
@@ -62,7 +34,6 @@ export class Tab1Page {
 
   public getRemoteDataAll() {
     return this.http.get('https://covid19.mathdro.id/api/daily/2-14-2020');
->>>>>>> 97c8d2ba6aea58fac49b13a9aecd08e676319768
   }
 
 }
